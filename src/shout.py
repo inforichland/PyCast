5b1b8dbd6fc7c4e0bd1e06e8a956de220851cce0
Tim Wawrzynczak <inforichland@gmail.com>
2011-05-23 21:43:27 -0500

    Fixed server so metadata is sent at the correct time

--- src/shout.py
+++ src/shout.py
@@ -15,6 +15,8 @@ import socket
 import array
 import os
 import sys
+import re
+import id3reader # available from pip
 
 try:
     import songsource
@@ -23,7 +25,7 @@ except ImportError:
     sys.exit(1)
 
 # set up logging
-logging.basicConfig(level=logging.DEBUG, format="%(created)-15s %(msecs)d %(levelname)8s %(thread)d %(name)s %(message)s")
+logging.basicConfig(level = logging.DEBUG, format = "%(created)-15s %(levelname)8s %(thread)d %(name)s %(message)s")
 log = logging.getLogger(__name__)
 
 # constants
@@ -31,45 +33,48 @@ BACKLOG = 10
 SIZE = 1024
 
 # address we're listening on
-interface='localhost'
-port=8888
+server_address = 'localhost'
+server_port = 8888
 
-# shoutcast stuff
-CHUNKSIZE=32*1024
-CHUNKS_IN_BUFFER=32
-MINIMUM_CHUNKS_IN_BUFFER=2
-RESPONSE= ["ICY 200 OK\r\n",
+# shoutcast / buffer constants
+CHUNKSIZE = 32*1024
+CHUNKS_IN_BUFFER = 32
+MINIMUM_BYTES_IN_BUFFER = 2*CHUNKSIZE
+RESPONSE = ["ICY 200 OK\r\n",
      "icy-notice1: <BR>This stream requires",
-     "icy-notice2: Python Shoutcast server<BR>\r\n",
+     "icy-notice2: Winamp, or another streaming media player<BR>\r\n",
      "icy-name: Python mix\r\n",
-     "icy-genre: Jazz Classical Prog Rock\r\n",
-     "icy-url: http://",interface,":",str(port),"\r\n",
+     "icy-genre: Jazz Classical Rock\r\n",
+     "icy-url: http://", server_address, ":", str(server_port), "\r\n",
      "content-type: audio/mpeg\r\n",
      "icy-pub: 1\r\n",
-     "icy-metaint: ",str(CHUNKSIZE), "\r\n",
+     "icy-metaint: ", str(CHUNKSIZE), "\r\n",
      "icy-br: 128\r\n\r\n"]
 
 class ShoutHandler(asyncore.dispatcher):
     """ This class handles the interaction with a client"""
 
     def __init__(self, client_socket, client_address):
-        self.songSource = songsource.DefaultSongSource() # get song source
+        #self.songSource = songsource.DefaultSongSource
+        self.songSource = songsource.OsWalkSongSource("/home/tim/Music/") # get song source
         self.client_address = client_address # (address,port) tuple of client
         fileName = self.songSource.nextSong() # get the next song from the song source
-        self.fd = open(fileName, 'rb') # open the next file
         self.data = array.array('B') # array to hold music data
         self.response = False # we haven't sent the initial response yet
-        self.file_size = os.path.getsize(fileName) # file size
-        self.chunks_in_buffer = 0 # there are no chunks in the buffer yet
+        self.wants_metadata = False # assume they don't want metadata
+        self.metadata_re = re.compile(r"Icy-MetaData", re.I)
+        self.bytes_to_metadata = CHUNKSIZE # how many bytes are left until we need to send metadata
 
         # initialize this dispatcher
-        self.refill_buffer() # refill the song buffer
+        self.getNextSong()
         asyncore.dispatcher.__init__(self, client_socket)
-        log.debug("created handler; waiting for loop")
 
-    # shouldn't really be reading from the client
+    # normally shouldn't really be reading much from the client.
+    # should be just once at the beginning (the request/headers),
+    #   and then once at the end when they disconnect (I believe this is for 
+    #   clients that send connection:close in the headers
     def readable(self):
-        return False
+        return True
 
     # Shoutcast servers should always be able to write data
     def writable(self):
@@ -79,29 +84,61 @@ class ShoutHandler(asyncore.dispatcher):
     def handle_read(self):
         data = self.recv(SIZE)
         if data:
-            log.debug("got data from client")
+            if self.metadata_re.search(data):
+                self.wants_metadata = True
+                log.debug("Client wants metadata")
+            else:
+                log.debug("Client sent: %s\n", data)
         else:
             log.debug("got null data")
 
+    # create the metadata string
+    def make_metadata(self):
+        text = "StreamTitle='%s';"
+        if self.id3:
+            text = text % self.id3.getValue('title')
+        else:
+            text = text % ''
+        blocks = len(text) // 16 + 1
+        metadata = chr(blocks) + text
+        metadata = metadata.ljust(blocks * 16 + 1, chr(0)) # add 1 to include the data length byte
+        return metadata.encode('ascii')
+
     # handle getting the next song from the song source
     def getNextSong(self):
-        fileName=self.songSource.nextSong() # get next song filename
-        self.fd = open(fileName, 'rb')
-        self.file_size = os.path.getsize(fileName)
-        self.refill_buffer()
+        try:
+            fileName = self.songSource.nextSong() # get next song filename
+            self.fd = open(fileName, 'rb')
+            self.file_size = os.path.getsize(fileName)
+            # if there is valid ID3 data, read it out of the file first,
+            # so we can skip sending it to the client
+            try:
+                self.id3 = id3reader.Reader(self.fd)
+                if isinstance(self.id3.header.size, int) and self.id3.header.size < self.file_size: # read out the id3 data
+                    self.fd.seek(self.id3.header.size+1, os.SEEK_SET)
+                    log.debug("Reading %d bytes of ID3: %d", self.id3.header.size, self.fd.tell())
+            except id3reader.Id3Error:
+                self.id3 = None
+            self.metadata = self.make_metadata()
+
+        except StopIteration:
+            fileName = None
+            self.fd = None
+        except IOError:
+            self.fd = None
+        log.debug("getting next song: %s", fileName)
 
     # refill the buffer
     def refill_buffer(self):
         try:
-            for i in range(0, CHUNKS_IN_BUFFER):
-                self.data.fromfile(self.fd, CHUNKSIZE)
-                self.data.append(0)
-                self.chunks_in_buffer += 1
+            if self.fd: # could be None
+                for i in range(0, CHUNKS_IN_BUFFER):
+                    self.data.fromfile(self.fd, CHUNKSIZE)
         except EOFError:
             self.fd.close()
-            self.getNextSong()            
+            self.getNextSong()
 
-    # writing to the file
+    # writing to the client
     def handle_write(self):
         # send the initial response
         if self.response == False:
@@ -110,31 +147,36 @@ class ShoutHandler(asyncore.dispatcher):
             self.response = True
         # send audio data
         else:
-            log.debug("writing to client")
-            if self.chunks_in_buffer < MINIMUM_CHUNKS_IN_BUFFER:
-                self.refill_buffer()                
-            self.send(self.data[0:CHUNKSIZE + 1])
-            # get rid of the chunk we just sent
-            self.data = self.data[CHUNKSIZE + 1:]
-            self.chunks_in_buffer -= 1
+            # figure out how much data there is to send and send it
+            data = self.data[0:self.bytes_to_metadata]
+            data_len = len(data)
+            self.bytes_to_metadata -= data_len
+            self.send(data)
+            
+            # send metadata
+            if self.bytes_to_metadata <= 0 and self.wants_metadata:
+                self.bytes_to_metadata = CHUNKSIZE
+                self.metadata = self.make_metadata()
+                self.send(self.metadata)
 
-  
+            # get rid of the chunk we just sent - this means the buffer for a client shouldn't exceed 1M in size
+            self.data = self.data[data_len:]
+            if len(self.data) < MINIMUM_BYTES_IN_BUFFER:
+                self.refill_buffer()
+
     # closing connection
     def handle_close(self):
         self.fd.close()
-        log.info("conn_closed: client_address=%s:%s" % \
-                     (self.client_address[0],
-                      self.client_address[1]))
+        log.info("lost client %s:%s" % (self.client_address[0], self.client_address[1]))
         self.close()
+
+    # handle errors
+    def handle_error(self):
+        pass
 
 class ShoutServer(asyncore.dispatcher):
     """This server uses ShoutHandler's to send Shoutcast data
 to clients."""
-
-    allow_reuse_address = True
-    request_queue_size = 5
-    address_family = socket.AF_INET
-    socket_type = socket.SOCK_STREAM
 
     # initialize the server
     def __init__(self, address, clientHandler=ShoutHandler):
@@ -142,28 +184,20 @@ to clients."""
         self.clientHandler = clientHandler
 
         asyncore.dispatcher.__init__(self)
-        self.create_socket(self.address_family,
-                           self.socket_type)
-
-        if self.allow_reuse_address:
-            self.set_reuse_addr()
-
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
         self.server_bind()
         self.server_activate()
 
     # bind to address / port
     def server_bind(self):
         self.bind(self.address)
-        log.debug("bind: address=%s:%s" % (self.address[0], self.address[1]))
+        log.debug("bind: %s:%s" % (self.address[0], self.address[1]))
 
     # start listening for clients
     def server_activate(self):
-        self.listen(self.request_queue_size)
-        log.debug("listen: backlog=%d" % self.request_queue_size)
-
-    # return our socket's file descriptor
-    def fileno(self):
-        return self.socket.fileno()
+        self.listen(BACKLOG)
+        log.debug("listen: %d" % BACKLOG)
 
     # start serving requests
     def serve(self):
@@ -171,29 +205,26 @@ to clients."""
 
     # handle a new client
     def handle_accept(self):
-        conn_socket, client_address = self.accept()
-        if self.verify_request(conn_socket, client_address):
-            self.process_request(conn_socket, client_address)
-
-    # all requests are valid
-    def verify_request(self, conn_sock, client_address):
-        return True
+        client_socket, client_address = self.accept()
+        self.process_request(client_socket, client_address)
 
     # start serving the file to the new client
-    def process_request(self, conn_socket, client_address):
-        log.info("conn_made: client_address=%s:%s" % \
-                     (client_address[0],
-                      client_address[1]))
-        self.clientHandler(conn_socket, client_address)
+    def process_request(self, client_socket, client_address):
+        log.info("new client: %s:%s" % (client_address[0], client_address[1]))
+        self.clientHandler(client_socket, client_address)
 
     # close the dispatcher when we're requested to close
     def handle_close(self):
         self.close()
 
+    # return our socket's file descriptor
+    def fileno(self):
+        return self.socket.fileno()
+
 # if we're run as a script, then just run the server
 if __name__ == '__main__':
     try:
-        server = ShoutServer((interface,port))
+        server = ShoutServer((server_address, server_port))
         server.serve()
     except KeyboardInterrupt:
         print "\nGoodbye!"
--- src/songsource.py
+++ src/songsource.py
@@ -4,6 +4,10 @@
 # Copyright: (c) Tim Wawrzynczak, MMXI
 # License: BSD 3-clause
 
+import os
+import fnmatch
+import re
+
 class DefaultSongSource(object):
     """Calling nextSong() on this will return a
 filename which the shoutcast server can use as the next file to play
@@ -14,3 +18,30 @@ to a client"""
 
     def nextSong(self):
         return self.defaultfile
+
+class OsWalkSongSource(object):
+    """Picks MP3 files in order (using os.walk) from a given directory as the song source."""
+
+    # a generator function to return files in root which match pattern
+    def all_files_by_pattern(self, pattern, root):
+        """Find all files in 'root' which match 'pattern' (matched with fnmatch)"""
+        for path, dirs, files in os.walk(os.path.abspath(root)):
+            for filename in fnmatch.filter(files, pattern):
+                yield os.path.join(path, filename)
+
+    def __init__(self, dir):
+        self.directory = dir
+        if not os.path.exists(dir):
+            self.files = None
+            raise
+        else:
+            self.files = self.all_files_by_pattern('*.mp3', dir)
+
+    # return the next song
+    def nextSong(self):
+        nextFile = self.files.next()
+        if nextFile == None:
+            self.files = self.all_files_by_pattern('*.mp3', dir)
+            return self.files.next()
+        else:
+            return nextFile
